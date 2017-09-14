eddf1ef68e56a7fc726e7bef5410cd538a6ee1e5
Jason Dixon <jason.dixon.email@gmail.com>
2017-09-14 22:45:48 +1200

    form and ip data retrieved

--- main.go
+++ main.go
@@ -6,33 +6,58 @@ import (
   "github.com/gin-gonic/gin"
 )
 
+// Shared info across all pages.
+const TITLE = "Balti Menu Order Form"
+
+
+// A single order
+type Order struct {
+  Meal string `form:"meal" binding:"required"`
+  Spice string `form:"spice" binding:"required"`
+  Naan string `form:"naan" binding:"required"`
+  Notes string `form:"notes"`
+}
+
+
+
 func main()  {
   // Set up our router
   router := gin.Default()
 
   // Load our template files and initialize some global variables
   router.LoadHTMLGlob("templates/*.html")
-  title := "Balti Menu Order"
 
   // Set up our routers
   router.GET("/", func(c *gin.Context) {
     // Menu
     c.HTML(http.StatusOK, "index.html", gin.H{
-      "title": title,
+      "title": TITLE,
     })
   })
   router.POST("/", func(c *gin.Context) {
-    fmt.Println("meal: ")
-    fmt.Println(c.PostForm("meal"))
+    var order Order
+    err := c.Bind(&order)
+    if err == nil {
+      fmt.Println("Your order:\n")
+      fmt.Println("meal:", order.Meal)
+      fmt.Println("spice:", order.Spice)
+      fmt.Println("naan:", order.Naan)
+      fmt.Println("notes:", order.Notes)
+    } else {
+      fmt.Println(err)
+    }
+
+    fmt.Println("Your IP is:", c.ClientIP())
+
     // Place order
     c.HTML(http.StatusOK, "order.html", gin.H{
-      "title": title,
+      "title": TITLE,
     })
   })
   router.GET("/view", func(c *gin.Context) {
     // View orders
     c.HTML(http.StatusOK, "view.html", gin.H{
-      "title": title,
+      "title": TITLE,
     })
   })
 
